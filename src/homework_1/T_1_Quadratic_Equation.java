52f17c29daf0330591b8b7c8160bc72b4ad395fa
Mariia Eki <mariiaeki@yandex.ru>
2019-12-03 16:30:43 +0300

    homework_1_0212

---
+++ src/homework_1/T_1_Quadratic_Equation.java
@@ -0,0 +1,27 @@
+package homework_1;
+
+public class T_1_Quadratic_Equation {
+    public static void main(String[] args) {
+        solver(2, 5, -7); // > 0
+        solver(1, -6, 9); // = 0
+        solver(9, -6, 2); // < 0
+    }
+
+    private static void solver(double a, double b, double c) {
+
+        double d = Math.pow(b, 2) - 4 * a * c;
+
+        if (d < 0) {
+            System.out.println("Корней на множестве действительных чисел нет");
+        } else if (d == 0) {
+            double x = -b / (2 * a);
+            System.out.println("x1 = x2 = " + x);
+        } else {
+            double x1 = (-b + Math.sqrt(d)) / (2 * a);
+            double x2 = (-b - Math.sqrt(d)) / (2 * a);
+            System.out.println("x1 = " + x1 + " ; " + "x2 = " + x2);
+        }
+
+
+    }
+}
